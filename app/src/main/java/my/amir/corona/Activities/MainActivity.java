6d21bee4506355f731a62dae151813374b45f167
Amirhossein Atashkar <ahatashkar@gmail.com>
2020-03-22 02:18:13 +0430

    initial

---
+++ app/src/main/java/my/amir/corona/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package my.amir.corona.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import my.amir.corona.R;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
