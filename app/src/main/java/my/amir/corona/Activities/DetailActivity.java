c555b2cee0d2b461b0d1aae239b8900de721dc01
Amirhossein Atashkar <ahatashkar@gmail.com>
2020-05-30 12:29:19 +0430

    remove graph

--- app/src/main/java/my/amir/corona/Activities/DetailActivity.java
+++ app/src/main/java/my/amir/corona/Activities/DetailActivity.java
@@ -47,8 +47,8 @@ public class DetailActivity extends AppCompatActivity implements AdapterView.OnI
     TextView recovered_textView;
     TextView per1m_textView;
     TextView date_textView;
-    GraphView graph;
-    Spinner spinner;
+//    GraphView graph;
+//    Spinner spinner;
 
     ProgressDialog dialog;
 
@@ -70,8 +70,8 @@ public class DetailActivity extends AppCompatActivity implements AdapterView.OnI
         recovered_textView = findViewById(R.id.recovered_textView);
         per1m_textView = findViewById(R.id.per1m_textView);
         date_textView = findViewById(R.id.date_textView);
-        graph = findViewById(R.id.graph);
-        spinner = findViewById(R.id.spinner);
+//        graph = findViewById(R.id.graph);
+//        spinner = findViewById(R.id.spinner);
 
         main_linearLayout.setVisibility(View.GONE);
 
@@ -79,8 +79,8 @@ public class DetailActivity extends AppCompatActivity implements AdapterView.OnI
                 R.array.graphItem_array, android.R.layout.simple_spinner_item);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+//        spinner.setAdapter(adapter);
+//        spinner.setOnItemSelectedListener(this);
 
 
         if (getSupportActionBar() != null){
@@ -103,7 +103,7 @@ public class DetailActivity extends AppCompatActivity implements AdapterView.OnI
             }
 
             getCountryDetail();
-            getCountryHistory();
+//            getCountryHistory();
         }
 
     }
@@ -123,9 +123,9 @@ public class DetailActivity extends AppCompatActivity implements AdapterView.OnI
          String type = (String) parent.getItemAtPosition(pos);
          Status.status status = Status.getGraphStatus(type);
 
-         if(status != null && list != null){
-             graphPlot(status);
-         }
+//         if(status != null && list != null){
+//             graphPlot(status);
+//         }
 
     }
 
@@ -195,151 +195,153 @@ public class DetailActivity extends AppCompatActivity implements AdapterView.OnI
 
     }
 
-    void getCountryHistory() {
 
-        Services service = Global.retrofit.create(Services.class);
-        Call<ResponseBody> call = service.getCountryHistory(countryName);
-
-        call.enqueue(new CallbackHandler<ResponseBody>() {
-            @Override
-            public void onSuccess(Response<ResponseBody> response) {
-
-                String string;
-                Gson gson = new Gson();
-
-                HistoryResponse historyResponse;
-
-                try {
-                    string = response.body().string();
-                    historyResponse = gson.fromJson(string, HistoryResponse.class);
-
-                    fillHistoryList(historyResponse.getStat_by_country());
-
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-
-            @Override
-            public void onError(String response) {
-
-                Log.v("error1", response);
-            }
-
-            @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-
-                Log.v("error1", t.toString());
-
-            }
-        });
-    }
-
-    void fillHistoryList(List<CountryDetail> originList){
-
-        String day = "";
-        list = new ArrayList<>();
-
-        for(int i=0; i<originList.size(); i++){
-            CountryDetail current = originList.get(i);
-            String currentDay = current.getRecord_date().substring(8,10);
-
-            if(!currentDay.equalsIgnoreCase(day)){
-                day = currentDay;
-                list.add(current);
-            }
-        }
-
-        graphPlot(Status.status.cases);
-
-
-    }
-
-    void graphPlot(Status.status status){
-
-        Date firstDate = Helper.getDate(list.get(0).getRecord_date());
-        Date lastDate = Helper.getDate(list.get(list.size()-1).getRecord_date());
-
-        double firstY;
-        double lastY;
-
-        switch (status){
-            case cases:
-                firstY = Helper.getFormattedStringToDouble(list.get(0).getTotal_cases());
-                lastY = Helper.getFormattedStringToDouble(list.get(list.size()-1).getTotal_cases());
-                break;
-
-            case death:
-                firstY = Helper.getFormattedStringToDouble(list.get(0).getTotal_deaths());
-                lastY = Helper.getFormattedStringToDouble(list.get(list.size()-1).getTotal_deaths());
-                break;
-
-            case active:
-                firstY = Helper.getFormattedStringToDouble(list.get(0).getActive_cases());
-                lastY = Helper.getFormattedStringToDouble(list.get(list.size()-1).getActive_cases());
-                break;
-
-            case recovered:
-                firstY = Helper.getFormattedStringToDouble(list.get(0).getTotal_recovered());
-                lastY = Helper.getFormattedStringToDouble(list.get(list.size()-1).getTotal_recovered());
-                break;
-        }
-
-        DataPoint[] dataPoints = new DataPoint[list.size()];
-
-        for(int i=0; i<list.size(); i++){
-            CountryDetail current = list.get(i);
-            Date date = Helper.getDate(current.getRecord_date());
-            if(date != null) {
-                DataPoint point = null;
-
-                switch (status){
-                    case recovered:
-                        point = new DataPoint(date, Helper.getFormattedStringToDouble(current.getTotal_recovered()));
-                        break;
-
-                    case active:
-                        point = new DataPoint(date, Helper.getFormattedStringToDouble(current.getActive_cases()));
-                        break;
-
-                    case death:
-                        point = new DataPoint(date, Helper.getFormattedStringToDouble(current.getTotal_deaths()));
-                        break;
-
-                    case cases:
-                        point = new DataPoint(date, Helper.getFormattedStringToDouble(current.getTotal_cases()));
-                        break;
-                }
-
-                dataPoints[i] = point;
-            }
-        }
-
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPoints);
-
-        graph.removeAllSeries();
-        graph.addSeries(series);
-
-        graph.setTitle("Total "+status.toString());
-
-        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(DetailActivity.this));
-//        graph.getGridLabelRenderer().setNumHorizontalLabels(6);
-        graph.getGridLabelRenderer().setHorizontalLabelsAngle(135);
-        graph.getGridLabelRenderer().setLabelHorizontalHeight(150);
-
-        if(firstDate != null && lastDate != null) {
-            graph.getViewport().setMinX(firstDate.getTime());
-            graph.getViewport().setMaxX(lastDate.getTime());
-            graph.getViewport().setXAxisBoundsManual(true);
-        }
-
-//        graph.getViewport().setMinY(firstY);
-//        graph.getViewport().setMaxY(lastY);
-//        graph.getViewport().setYAxisBoundsManual(true);
-
-        graph.getGridLabelRenderer().setHumanRounding(true);
-    }
+    // get History and plot a graph
+//    void getCountryHistory() {
+//
+//        Services service = Global.retrofit.create(Services.class);
+//        Call<ResponseBody> call = service.getCountryHistory(countryName);
+//
+//        call.enqueue(new CallbackHandler<ResponseBody>() {
+//            @Override
+//            public void onSuccess(Response<ResponseBody> response) {
+//
+//                String string;
+//                Gson gson = new Gson();
+//
+//                HistoryResponse historyResponse;
+//
+//                try {
+//                    string = response.body().string();
+//                    historyResponse = gson.fromJson(string, HistoryResponse.class);
+//
+//                    fillHistoryList(historyResponse.getStat_by_country());
+//
+//
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//
+//
+//            @Override
+//            public void onError(String response) {
+//
+//                Log.v("error1", response);
+//            }
+//
+//            @Override
+//            public void onFailure(Call<ResponseBody> call, Throwable t) {
+//
+//                Log.v("error1", t.toString());
+//
+//            }
+//        });
+//    }
+//
+//    void fillHistoryList(List<CountryDetail> originList){
+//
+//        String day = "";
+//        list = new ArrayList<>();
+//
+//        for(int i=0; i<originList.size(); i++){
+//            CountryDetail current = originList.get(i);
+//            String currentDay = current.getRecord_date().substring(8,10);
+//
+//            if(!currentDay.equalsIgnoreCase(day)){
+//                day = currentDay;
+//                list.add(current);
+//            }
+//        }
+//
+//        graphPlot(Status.status.cases);
+//
+//
+//    }
+//
+//    void graphPlot(Status.status status){
+//
+//        Date firstDate = Helper.getDate(list.get(0).getRecord_date());
+//        Date lastDate = Helper.getDate(list.get(list.size()-1).getRecord_date());
+//
+//        double firstY;
+//        double lastY;
+//
+//        switch (status){
+//            case cases:
+//                firstY = Helper.getFormattedStringToDouble(list.get(0).getTotal_cases());
+//                lastY = Helper.getFormattedStringToDouble(list.get(list.size()-1).getTotal_cases());
+//                break;
+//
+//            case death:
+//                firstY = Helper.getFormattedStringToDouble(list.get(0).getTotal_deaths());
+//                lastY = Helper.getFormattedStringToDouble(list.get(list.size()-1).getTotal_deaths());
+//                break;
+//
+//            case active:
+//                firstY = Helper.getFormattedStringToDouble(list.get(0).getActive_cases());
+//                lastY = Helper.getFormattedStringToDouble(list.get(list.size()-1).getActive_cases());
+//                break;
+//
+//            case recovered:
+//                firstY = Helper.getFormattedStringToDouble(list.get(0).getTotal_recovered());
+//                lastY = Helper.getFormattedStringToDouble(list.get(list.size()-1).getTotal_recovered());
+//                break;
+//        }
+//
+//        DataPoint[] dataPoints = new DataPoint[list.size()];
+//
+//        for(int i=0; i<list.size(); i++){
+//            CountryDetail current = list.get(i);
+//            Date date = Helper.getDate(current.getRecord_date());
+//            if(date != null) {
+//                DataPoint point = null;
+//
+//                switch (status){
+//                    case recovered:
+//                        point = new DataPoint(date, Helper.getFormattedStringToDouble(current.getTotal_recovered()));
+//                        break;
+//
+//                    case active:
+//                        point = new DataPoint(date, Helper.getFormattedStringToDouble(current.getActive_cases()));
+//                        break;
+//
+//                    case death:
+//                        point = new DataPoint(date, Helper.getFormattedStringToDouble(current.getTotal_deaths()));
+//                        break;
+//
+//                    case cases:
+//                        point = new DataPoint(date, Helper.getFormattedStringToDouble(current.getTotal_cases()));
+//                        break;
+//                }
+//
+//                dataPoints[i] = point;
+//            }
+//        }
+//
+//        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPoints);
+//
+//        graph.removeAllSeries();
+//        graph.addSeries(series);
+//
+//        graph.setTitle("Total "+status.toString());
+//
+//        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(DetailActivity.this));
+////        graph.getGridLabelRenderer().setNumHorizontalLabels(6);
+//        graph.getGridLabelRenderer().setHorizontalLabelsAngle(135);
+//        graph.getGridLabelRenderer().setLabelHorizontalHeight(150);
+//
+//        if(firstDate != null && lastDate != null) {
+//            graph.getViewport().setMinX(firstDate.getTime());
+//            graph.getViewport().setMaxX(lastDate.getTime());
+//            graph.getViewport().setXAxisBoundsManual(true);
+//        }
+//
+////        graph.getViewport().setMinY(firstY);
+////        graph.getViewport().setMaxY(lastY);
+////        graph.getViewport().setYAxisBoundsManual(true);
+//
+//        graph.getGridLabelRenderer().setHumanRounding(true);
+//    }
 }
